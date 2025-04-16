e8e0a78dd5e7f8296f7ee7e14de9577e9d1fae85
SebastianJedrak <jedrak.sebastian@gmail.com>
2025-04-16 19:33:00 +0200

    feat: Pricing page structure

--- src/components/pages/Pricing.tsx
+++ src/components/pages/Pricing.tsx
@@ -1,7 +1,20 @@
+import PriceBox from "../UI/PriceBox";
 import "./Pricing.scss";
 
 const Pricing: React.FC = () => {
-  return <div className="pricing-page">Pricing</div>;
+  return (
+    <div className="pricing-page">
+      <h3>Simple & flexible pricing built for everyone</h3>
+      <p className="paragraph">
+        Start with 14-day free trial. No credit card needed. Cancel at anytime.
+      </p>
+      <div className="price-box__container">
+        <PriceBox />
+        <PriceBox />
+        <PriceBox />
+      </div>
+    </div>
+  );
 };
 
 export default Pricing;
