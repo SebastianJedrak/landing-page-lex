e0532ce1add611fe80ff8f1d5bc76a9fec07ba99
SebastianJedrak <jedrak.sebastian@gmail.com>
2025-04-16 19:52:36 +0200

    feat: prepare PriceBox

--- src/components/UI/PriceBox.tsx
+++ src/components/UI/PriceBox.tsx
@@ -1,7 +1,47 @@
+import { Features } from "../pages/Pricing";
+import Button from "./Button";
+import Feature from "./Feature";
 import "./PriceBox.scss";
 
-const PriceBox: React.FC = () => {
-  return <div>PriceBox</div>;
+interface PriceBoxProps {
+  planName: string;
+  price: string;
+  description: string;
+  features: Features;
+  special?: boolean;
+}
+
+const PriceBox: React.FC<PriceBoxProps> = ({
+  planName,
+  price,
+  description,
+  features,
+  special = false,
+}) => {
+  return (
+    <div className="price-box__container">
+      <div className="price-box__card">
+        <p>{planName}</p>
+        <div className="price-box__card__price">
+          <span>$</span>
+          <h2>{price}</h2>
+          <span>per user</span>
+          <span>per month</span>
+        </div>
+        <p>{description}</p>
+      </div>
+
+      <div className="price-box__features">
+        {features.map((feature) => (
+          <div key={feature.id} className="price-box__features__item">
+            <Feature />
+          </div>
+        ))}
+
+        <Button label="Start Free Trial" />
+      </div>
+    </div>
+  );
 };
 
 export default PriceBox;
--- src/components/pages/Pricing.tsx
+++ src/components/pages/Pricing.tsx
@@ -1,6 +1,21 @@
 import PriceBox from "../UI/PriceBox";
 import "./Pricing.scss";
 
+export type Feature = {
+  id: number;
+  name: string;
+  active: boolean;
+};
+
+export type Features = Feature[];
+
+const featuresNames = [
+  "2 GB of hosting space",
+  "14 days of free backups",
+  "Social integrations",
+  "Advanced client billing",
+];
+
 const Pricing: React.FC = () => {
   return (
     <div className="pricing-page">
@@ -8,10 +23,41 @@ const Pricing: React.FC = () => {
       <p className="paragraph">
         Start with 14-day free trial. No credit card needed. Cancel at anytime.
       </p>
-      <div className="price-box__container">
-        <PriceBox />
-        <PriceBox />
-        <PriceBox />
+      <div className="price-boxes__container">
+        <PriceBox
+          planName="START"
+          price="19"
+          description="All the features you need to keep your personal files safe, accessible, and easy to share."
+          features={[
+            { id: 1, name: featuresNames[0], active: true },
+            { id: 2, name: featuresNames[1], active: true },
+            { id: 3, name: featuresNames[2], active: false },
+            { id: 4, name: featuresNames[3], active: false },
+          ]}
+        />
+        <PriceBox
+          planName="ENTERPRISE"
+          price="49"
+          description="All the features you need to keep your personal files safe, accessible, and easy to share."
+          features={[
+            { id: 1, name: featuresNames[0], active: true },
+            { id: 2, name: featuresNames[1], active: true },
+            { id: 3, name: featuresNames[2], active: true },
+            { id: 4, name: featuresNames[3], active: false },
+          ]}
+          special
+        />
+        <PriceBox
+          planName="ENTERPRISE"
+          price="99"
+          description="All the features you need to keep your personal files safe, accessible, and easy to share."
+          features={[
+            { id: 1, name: featuresNames[0], active: true },
+            { id: 2, name: featuresNames[1], active: true },
+            { id: 3, name: featuresNames[2], active: true },
+            { id: 4, name: featuresNames[3], active: true },
+          ]}
+        />
       </div>
     </div>
   );
